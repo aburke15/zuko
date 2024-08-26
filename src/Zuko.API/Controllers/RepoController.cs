170df392bf837fa9a1f56bb2ea0babec8b6e6b53
Andre Burke <good_credits.08@icloud.com>
2024-08-26 17:23:38 -0600

    first commit

---
+++ src/Zuko.API/Controllers/RepoController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Zuko.API.Controllers
+{
+    [Route("api/repos")]
+    [ApiController]
+    public class RepoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetRepos()
+        {
+            List<RepoResponse> repos =
+            [
+                new () { Id = 1, Name = "Iroh" },
+                new () { Id = 2, Name = "Zuko" },
+                new () { Id = 3, Name = "test-ninja" },
+                new () { Id = 4, Name = "backend" },
+            ];
+
+            await Task.Delay(100);
+
+            return Ok(repos);
+        }
+
+        public class RepoResponse
+        {
+            [JsonProperty("id")]
+            public int Id { get; set; }
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("description")]
+            public string Description { get; set; }
+            [JsonProperty("html_url")]
+            public string Link { get; set; } 
+            [JsonProperty("created_at")]
+            public string CreatedAt { get; set; } 
+            [JsonProperty("language")]
+            public string Language { get; set; } 
+            [JsonProperty("private")]
+            public bool Private { get; set; }
+            [JsonProperty("updated_at")]
+            public string UpdatedAt { get; set; } 
+        }
+    }
+}
---
+++ tests/Zuko.API.Tests/RepoControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using Zuko.API.Controllers;
+
+namespace Zuko.API.Tests;
+
+[TestFixture]
+public class RepoControllerTests
+{
+    [Test]
+    public async Task GetRepos_WhenCalled_ReturnsActionResult()
+    {
+        var controller = new RepoController();
+
+        var result = await controller.GetRepos();
+
+        Assert.That(result, Is.InstanceOf<IActionResult>());
+    }
+}
